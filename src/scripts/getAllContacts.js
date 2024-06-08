06df026c95d2a46205a4176e7e49ae615b79e3e1
Denis <122015463+iziKomSD@users.noreply.github.com>
2024-06-08 12:42:10 +0300

    +2 task

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -2,8 +2,12 @@ import { PATH_DB } from '../constants/contacts.js';
 import fs from 'fs/promises';
 
 export const getAllContacts = async () => {
-  const allContacts = await fs.readFile(PATH_DB, 'utf-8');
-  return JSON.parse(allContacts);
+  try {
+    const allContacts = await fs.readFile(PATH_DB, 'utf-8');
+    return JSON.parse(allContacts);
+  } catch (error) {
+    console.error('Error:', error);
+  }
 };
 
 console.log(await getAllContacts());
