5c3ee0252df98c5bea793c3357381f5dfe5ad9af
Denis <122015463+iziKomSD@users.noreply.github.com>
2024-06-08 12:46:45 +0300

    6tasks

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,5 +1,17 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
 
-export const removeAllContacts = async () => {};
+export const removeAllContacts = async () => {
+  try {
+    const deletedContacts = [];
+    await fs.writeFile(
+      PATH_DB,
+      JSON.stringify(deletedContacts, null, 2),
+      'utf-8',
+    );
+  } catch (error) {
+    console.error('Error:', error);
+  }
+};
 
 await removeAllContacts();
