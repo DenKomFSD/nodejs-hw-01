ae9531c5fc4a92d3c4842d40d7130a039bc58124
Denis <122015463+iziKomSD@users.noreply.github.com>
2024-06-08 18:49:57 +0300

    7 task

--- src/scripts/thanos.js
+++ src/scripts/thanos.js
@@ -1,5 +1,16 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
 
-export const thanos = async () => {};
+export const thanos = async () => {
+  let contactsArr;
+  try {
+    const curContacts = await fs.readFile(PATH_DB, 'utf-8');
+    contactsArr = JSON.parse(curContacts);
+    contactsArr = contactsArr.filter(() => Math.random() > 0.5);
+    await fs.writeFile(PATH_DB, JSON.stringify(contactsArr, null, 2), 'utf-8');
+  } catch (error) {
+    console.error('Error:', error);
+  }
+};
 
 await thanos();
