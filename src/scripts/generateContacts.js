6666986f0311321d67d509f9b533a81fc680f20e
Denis <122015463+iziKomSD@users.noreply.github.com>
2024-06-08 09:20:07 +0300

    step 2

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,16 +1,21 @@
-import { PATH_DB } from "../constants/contacts.js";
-import fs from "fs/promises";
-import { createFakeContact } from "../utils/createFakeContact.js";
+import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
+import { createFakeContact } from '../utils/createFakeContact.js';
 
 const generateContacts = async (number) => {
-  let contacts = [];
   try {
-    //read existing contacts
-    const data = await fs.readFile(PATH_DB, "utf-8");
-    const contacts = JSON.parse(data);
+    const existContacts = await fs.readFile(PATH_DB, 'utf-8');
+    const allContacts = JSON.parse(existContacts);
 
-    //generate new contacts
-  } catch (error) {}
+    for (let i = 0; i < number; i++) {
+      allContacts.push(createFakeContact());
+    }
+
+    //write to array
+    await fs.writeFile(PATH_DB, JSON.stringify(allContacts), 'utf-8');
+  } catch (error) {
+    console.error('Error:', error);
+  }
 };
 
 await generateContacts(5);
